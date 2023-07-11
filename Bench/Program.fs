28a85e6a80d492ffde4c36382748ce66c6f5897d
Mark Lambert <marklam@spunow.co.uk>
2023-07-11 13:44:40 +0100

    BenchmarkDotNet vs Paket issue repro

---
+++ Bench/Program.fs
@@ -0,0 +1,33 @@
+﻿open System
+open System.Security.Cryptography
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Jobs
+open BenchmarkDotNet.Running
+
+[<SimpleJob(RuntimeMoniker.Net60, baseline = true)>]
+type Md5VsSha256() =
+    let sha256 = SHA256.Create()
+    let md5 = MD5.Create()
+    let mutable data = Array.empty<byte>
+
+    [<Params(1000, 10000)>]
+    [<DefaultValue>]
+    val mutable N : int
+
+    [<GlobalSetup>]
+    member this.Setup() =
+        data <- Array.zeroCreate this.N
+        Random(42).NextBytes(data)
+
+    [<Benchmark>]
+    member _.Sha256() =
+        sha256.ComputeHash(data)
+
+    [<Benchmark>]
+    member _.Md5() =
+        md5.ComputeHash(data)
+
+[<EntryPoint>]
+let main _argv =
+    BenchmarkRunner.Run<Md5VsSha256>() |> ignore
+    0
